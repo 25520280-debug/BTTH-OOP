4ac065466873eef43bb3935c0d9a5b28f3d03368
25520280-debug <25520280@gm.uit.edu.vn>
2026-03-19 16:31:07 +0700

    Adding comments in Lab1+2+3

--- BTTH-OOP/BTTH-1/Lab_1-2-3/frac.h
+++ BTTH-OOP/BTTH-1/Lab_1-2-3/frac.h
@@ -1,44 +1,158 @@
-#pragma once
-
-// Khai báo lớp phân số.
+#pragma once // Tránh lỗi lặp định nghĩa.
+/*
+    Tên lớp : frac
+    ------------------------------------------------------
+    Nội dung :
+        - a (long long) : Tử số.
+        - b (long long) : Mẫu số.
+    ------------------------------------------------------  
+    Tính năng của phương thức :
+        - setFrac : Thay giá trị cho phân số.
+        - getNmr, getDmn : Đọc và trả về giá trị của tử
+        số và mẫu số.
+*/
 class frac {
 private:
-    // Khai báo tử số và mẫu số.
     long long a, b;
 public:
-    // Phương thức cài đặt phân số.
     void setFrac(long long x, long long y);
-    // Phương thức đọc giá trị của tử và mẫu của phân số.
     long long getNmr();
     long long getDmn();
 };
+/*  
+    Tên hàm : simplyFrac
+    ------------------------------------------------------
+    Input :
+        - a (long long) : Tử số.
+        - b (long long) : Mẫu số.
+    ------------------------------------------------------
+    Output :
+        Không trả về giá trị.
+    ------------------------------------------------------
+    Tính năng :
+        - Hàm thay đổi giá trị của a và b về dạng rút gọn
+        của chúng.
+        - Hàm rút gọn phân số bằng cách lần lượt chia cả tử
+        và mẫu với ước chung lớn nhất của chúng.
 
-/*
-    Hàm rút gọn phân số bằng cách lần lượt
-    chia cả tử và mẫu với ước chung lớn nhất của chúng.
 */
 void simplyFrac(long long &a, long long &b);
-
-// Hàm nhập phân số.
+/*
+    Tên hàm : Input
+    ------------------------------------------------------
+    Input :
+        - x (frac) : Phân số mới cần nhập giá trị.
+    ------------------------------------------------------
+    Output :
+        - Không trả về giá trị.
+    ------------------------------------------------------  
+    Tính năng :
+        - Thay giá trị được nhập từ bàn phím vào phân số
+        cần nhập giá trị.
+*/
 void Input(frac &x);
-
-// Hàm xuất và trả về phân số đúng dạng.
+/*
+    Tên hàm : Output
+    ------------------------------------------------------
+    Input :
+        - a (long long) : Tử số.
+        - b (long long) : Mẫu số.
+    ------------------------------------------------------
+    Output :
+        - Phân số có tử số a và mẫu số b dưới dạng a/b.
+    ------------------------------------------------------  
+    Tính năng :
+        - Xuất ra màn hình phân số đúng định dạng toán học.
+*/
 void Output(long long a, long long b);
+/*
+    Tên hàm : sumFrac
+    ------------------------------------------------------
+    Input :
+        - i (frac) : Phân số đầu tiên.
+        - j (frac) : Phân số thứ hai.
+    ------------------------------------------------------
+    Output :
+        - Tổng của hai phân số i và j dưới dạng a/b.
+    ------------------------------------------------------  
+    Tính năng :
+        - Tính toán tổng của hai phân số i và j bằng cách
+        quy đồng.
 
-// Hàm tính tổng hai phân số.
+*/
 void sumFrac(frac i, frac j);
-
-// Hàm tính hiệu hai phân số.
+/*
+    Tên hàm : difFrac
+    ------------------------------------------------------
+    Input :
+        - i (frac) : Phân số đầu tiên.
+        - j (frac) : Phân số thứ hai.
+    ------------------------------------------------------
+    Output :
+        - Hiệu của hai phân số i và j dưới dạng a/b.
+    ------------------------------------------------------  
+    Tính năng :
+        - Tính toán hiệu của hai phân số i và j bằng cách
+        quy đồng.
+*/
 void difFrac(frac i, frac j);
-
-// Hàm tính tích hai phân số.
+/*
+    Tên hàm : prodFrac
+    ------------------------------------------------------
+    Input :
+        - i (frac) : Phân số đầu tiên.
+        - j (frac) : Phân số thứ hai.
+    ------------------------------------------------------
+    Output :
+        - Tích của hai phân số i và j dưới dạng a/b.
+    ------------------------------------------------------  
+    Tính năng :
+        - Tính toán tích của hai phân số i và j.
+*/
 void prodFrac(frac i, frac j);
+/*
+    Tên hàm : quoFrac
+    ------------------------------------------------------
+    Input :
+        - i (frac) : Phân số đầu tiên.
+        - j (frac) : Phân số thứ hai.
+    ------------------------------------------------------
+    Output :
+        - Thương của hai phân số i và j dưới dạng a/b.
+    ------------------------------------------------------  
+    Tính năng :
+        - Tính toán thương của hai phân số i và j.
 
-// Hàm tính thương hai phân số.
+*/
 void quoFrac(frac i, frac j);
-
-// Hàm xuất kết quả tổng, hiệu, tích, thương của hai phân số.
+/*
+    Tên hàm : outCalFrac
+    ------------------------------------------------------
+    Input :
+        - i (frac) : Phân số đầu tiên.
+        - j (frac) : Phân số thứ hai.
+    ------------------------------------------------------
+    Output :
+        - Tổng, hiệu, tích, thương của hai phân số i và j
+        dưới dạng a/b.
+    ------------------------------------------------------  
+    Tính năng :
+        - Xuất ra màn hình tổng, hiệu, tích, thương của hai
+        phân số.
+*/
 void outCalFrac(frac i, frac j);
-
-// Hàm so sánh và trả về phân số lớn nhất.
+/*
+    Tên hàm : maxFrac
+    ------------------------------------------------------
+    Input :
+        - i (frac) : Phân số đầu tiên.
+        - j (frac) : Phân số thứ hai.
+    ------------------------------------------------------
+    Output :
+        - Phân số lớn nhất dưới dạng a/b.
+    ------------------------------------------------------  
+    Tính năng :
+        - Tìm phân số lớn nhất bằng cách quy đồng đưa bài
+        toán về dạng so sánh hai số nguyên.
+*/
 void maxFrac(frac i, frac j);
