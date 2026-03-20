377cea232af28ee5a8f3cf6d0ef3e32044b0ab2c
25520280-debug <25520280@gm.uit.edu.vn>
2026-03-20 23:08:54 +0700

    Adding comments to Lab4.

--- BTTH-OOP/BTTH-1/Lab_4/day.h
+++ BTTH-OOP/BTTH-1/Lab_4/day.h
@@ -1,30 +1,109 @@
-#pragma once
+#pragma once // Tránh lỗi lặp định nghĩa.
 
 /*
-
+    Tên lớp : doy (day of year)
+    ------------------------------------------------------
+    Nội dung :
+        - d (int) : Ngày.
+        - m (int) : Tháng.
+        - y (int) : Năm.
+    ------------------------------------------------------  
+    Tính năng của phương thức :
+        - setDOY : Thay giá trị cho ngày, tháng, năm.
+        - getDay, getMonth, getYear : Đọc và trả về giá 
+        trị của ngày, tháng, và năm.
 */
 class doy
 {
     private:
-    // Khai báo ngày, tháng, và năm.
         int d, m, y;
     public:
-    // Phương thức cài đặt ngày, tháng, và năm.
         void setDOY(int day, int month, int year);
-    // Phương thức đọc ngày, tháng, và năm.
         int getDay();
         int getMonth();
         int getYear();
 };
-// Hàm kiểm tra tính hợp lệ của ngày, tháng, và năm.
+/*  
+    Tên hàm : check
+    ------------------------------------------------------
+    Input :
+        - d (int) : Ngày.
+        - m (int) : Tháng.
+        - y (int) : Năm.
+    ------------------------------------------------------
+    Output :
+        true hoặc false.
+    ------------------------------------------------------
+    Tính năng :
+        - Hàm giúp kiểm tra ngày, tháng, năm đã được nhập 
+        đúng hay chưa.
+*/
 bool check (int day, int month, int year);
-// Hàm kiểm tra năm nhuận.
+/*  
+    Tên hàm : checkLeap
+    ------------------------------------------------------
+    Input :
+        - y (int) : Năm.
+    ------------------------------------------------------
+    Output :
+        true hoặc false.
+    ------------------------------------------------------
+    Tính năng :
+        - Hàm giúp kiểm tra năm đã được nhập có phải năm
+        nhuận hay không.
+*/
 bool checkLeap (int year);
-// Hàm nhập ngày, tháng, và năm.
+/*  
+    Tên hàm : Input
+    ------------------------------------------------------
+    Input :
+        - x (doy) : Ngày trong năm.
+    ------------------------------------------------------
+    Output :
+        Không trả về giá trị.
+    ------------------------------------------------------
+    Tính năng :
+        - Thay giá trị được nhập từ bàn phím vào phân số
+        cần nhập giá trị.
+*/
 void Input (doy &x);
-// Hàm trả về đúng định dạng ngày, tháng, và năm.
+/*
+    Tên hàm : Output
+    ------------------------------------------------------
+    Input :
+        - x (doy) : Ngày trong năm.
+    ------------------------------------------------------
+    Output :
+        - Ngày trong năm có định dạng day/month/year.
+    ------------------------------------------------------  
+    Tính năng :
+        - Xuất ra màn hình ngày trong năm đúng định dạng.
+*/
 void Output (doy x);
-// Hàm trả về giới hạn số ngày trong tháng.
+/*
+    Tên hàm : limitDayinMonth
+    ------------------------------------------------------
+    Input :
+        - m (int) : Tháng.
+        - y (int) : Năm.
+    ------------------------------------------------------
+    Output :
+        - Số ngày có trong tháng được nhập.
+    ------------------------------------------------------  
+    Tính năng :
+        - Cho biết tháng đó có bao nhiêu ngày.
+*/
 int limitDayinMonth (int month, int year);
-// Hàm tịnh tiến sang ngày kế tiếp.
+/*
+    Tên hàm : addDay
+    ------------------------------------------------------
+    Input :
+        - x (doy) : Ngày trong năm.
+    ------------------------------------------------------
+    Output :
+        - Không trả về giá trị.
+    ------------------------------------------------------  
+    Tính năng :
+        - Tăng ngày được nhập lên 1 đơn vị ngày.
+*/
 void addDay(doy &x);
