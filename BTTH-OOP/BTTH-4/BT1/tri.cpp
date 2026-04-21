2e88336a14455754fbdce4d39ddee7f6558f53c6
25520280-debug <25520280@gm.uit.edu.vn>
2026-04-21 18:13:10 +0700

    Update BTTH-3 - BT1.

--- BTTH-OOP/BTTH-4/BT1/tri.cpp
+++ BTTH-OOP/BTTH-4/BT1/tri.cpp
@@ -13,16 +13,72 @@ class vert {
             in >> p.x >> p.y;
             return in;
         }
+        void InputVertex(vert& x) {
+            while (true) {
+                if (cin >> x) break;
+                else {
+                    cout << "Error! Please try again!\n";
+                    cin.clear();
+                    cin.ignore(10000,'\n');
+        }
+    }
+}
 };
 
-class edge : protected vert{
+class edge {
     private:
         vert a;
         vert b;
     public:
-        void Input() {
-            cout << "Enter the coordinate of first vertex : "; cin >> a;
-            cout << "\nEnter the coordinate of second vertex : "; cin >> b;
-        }
+        edge(vert x, vert y) : a(x), b(y) {}
         const double getLength() {return sqrtf((a.x - b.x)*(a.x - b.x) + (a.y - b.y)*(a.y - b.y));}
+};
+
+class tri : protected vert{
+    private :
+        edge a;
+        edge b;
+        edge c;
+        double AB = c.getLength(), AC = b.getLength(), BC = a.getLength();
+    public:
+        double p = (AB+AC+BC)/2;
+        double check = p * (p - AB)*(p - AC)*(p - BC);
+
+        void Input() {
+            while (true) {
+                vert x, y, z;
+                cout << "Please enter the first vertex : "; InputVertex(x);
+                cout << "\nPlease enter the second vertex : "; InputVertex(y);
+                cout << "\nPlease enter the third vertex : "; InputVertex(z); 
+                c = edge(x,y), a = edge(y,z), b = edge(z,x);
+                if (check > 0) break; 
+            }
+        }
+
+        void triType() {
+            if (AB - AC - BC == 0) {
+                cout << "This is an equilateral triangle.\n";
+                return;
+            }
+            else if ((AB - AC == 0) || (AB - BC == 0) || (AC - BC == 0)) {
+                cout << "This is an isosceles triangle.\n";
+                return;
+            }
+            else if ((AB*AB + AC*AC == BC*BC)||(AB*AB + BC*BC == AC*AC)||(BC*BC+ AC*AC == AB*AB)) {
+                cout << "This is an right triangle.\n";
+                return;
+            }
+            else {
+                cout << "This is an scalene triangle.\n";
+                return;
+            }
+        }
+
+        double triCir() { return p*2; }
+
+        double triArea() {
+            return sqrtf(p * (p - AB)*(p - AC)*(p - BC));
+        }
+
+        
 };
