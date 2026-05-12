95a6b7fe156ad5dba27dd3456bd65a4910b499d5
25520280-debug <25520280@gm.uit.edu.vn>
2026-05-12 21:36:38 +0700

    Update BT5 - main.cpp.

--- BTTH-OOP/BTTH-4/BT5/main.cpp
+++ BTTH-OOP/BTTH-4/BT5/main.cpp
@@ -4,14 +4,13 @@ int main() {
     Polynomial p1;
     p1.input();
 
-    cout << "P1(x) = ";
-    p1.display();
+    cout << "P1(x) = " << p1 << endl;          
 
     do {
         double x;
         cout << "Please enter the value for x: ";
         cin >> x;
-        cout << "P1(" << x << ") = " << p1.evaluate(x) << endl;
+        cout << "P1(" << x << ") = " << p1(x) << endl;   
     } while (askYesNo("Do you want to evaluate P1 at another value of x?"));
 
     if (!askYesNo("Do you want to continue with a second polynomial?")) {
@@ -21,23 +20,21 @@ int main() {
     Polynomial p2;
     p2.input();
 
-    cout << "P2(x) = ";
-    p2.display();
+    cout << "P2(x) = " << p2 << endl;          
 
     char op;
     cout << "Do you want to add or subtract? (+ / -): ";
     cin >> op;
 
-    Polynomial result = (op == '+') ? p1.add(p2) : p1.subtract(p2);
+    Polynomial result = (op == '+') ? p1 + p2 : p1 - p2;   
 
-    cout << "Result: P(x) = ";
-    result.display();
+    cout << "Result: P(x) = " << result << endl;    
 
     if (askYesNo("Do you want to evaluate the result at a value of x?")) {
         double x;
         cout << "Please enter the value for x: ";
         cin >> x;
-        cout << "P(" << x << ") = " << result.evaluate(x) << endl;
+        cout << "P(" << x << ") = " << result(x) << endl;  
     }
 
     return 0;
